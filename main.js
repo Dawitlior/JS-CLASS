401b3c863b287c19dccb0dec3399ebbe2f094736
Dawitlior <dawitlior777@gmail.com>
2022-06-20 08:33:16 +0300

    add

--- main.js
+++ main.js
@@ -23,19 +23,18 @@
 // // console.log(new Student("lior", 11, 1992314));
 // // console.log(new Student(`yaso`, 10, 232122));
 // 4.
-// class User{
-
-//     constructor(firtsName,lastName,age,email){
-//         this.firtsName = firtsName;
-//         this.lastName = lastName;
-//         this.age = age;
-//         this.email = email;
-//     }
-// }
-//  for(const userKey in User){
-//     document.getElementById("id_h3").innerHTML +=
-//     new User`{userKey} : ${User[userKey]}}`
-//   }
+class User {
+  constructor(firtsName, lastName, age, email) {
+    this.firtsName = firtsName;
+    this.lastName = lastName;
+    this.age = age;
+    this.email = email;
+  }
+}
+btn.onclick = () => {
+  let one = new User(firtsName.value, lastName.value, age1.value, email.value);
+  tbody.innerHTML = `<tr><td>${one.firtsName}</td><td>${one.lastName}</td><td>${one.age}</td><td>${one.email}</td></tr>`;
+};
 
 7;
 class Dog {
@@ -44,19 +43,19 @@ class Dog {
     this.type = type;
     this.date = date;
   }
-};
-8
+}
+8;
 let dog1 = new Dog("alaila", "pitbull", "20/2/1992");
 let dog2 = new Dog("avi", "pitbull", "20/2/1992");
 let dog3 = new Dog("ori", "pitbull", "20/2/1992");
 let dog4 = new Dog("ali", "pitbull", "20/2/1992");
 const myArray = [dog1, dog2, dog3, dog4];
 console.log(myArray);
- myArray.forEach((i) => {
-   console.log(i);
- });
+myArray.forEach((i) => {
+  console.log(i);
+});
 
-9.
+9;
 // class Teacher{
 //     constructor(firstName,lastName,claas,email){
 //         this.firstName = firstName;
@@ -66,7 +65,4 @@ console.log(myArray);
 //     }
 // }
 
-
-
-11.
-
+11;
